40e39aeafe46c478e8767636732f7b6446cedb61
perlisngankam2 <perliss.ngankam@gmail.com>
2023-04-17 19:49:37 +0200

    ajout du  module parents

---
+++ front/pages/parents/addParents.jsx
@@ -0,0 +1,194 @@
+import { AlertDialogCloseButton, Box, Heading } from "@chakra-ui/react";
+import SearchBar from "../../components/atoms/searchbar";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  useDisclosure,
+  Button,
+  Center,
+  Flex,
+  Input,
+  FormControl,
+  FormLabel,
+  extendTheme,
+  Icon,
+  useToast
+} from '@chakra-ui/react';
+import React from "react";
+import { IoIosAdd } from "react-icons/io";
+import {useMutation } from '@apollo/client';
+import { CREATE_SECTION, UPDATA_SECTION} from "../../graphql/Mutation";
+import { GET_ALL_SECTION } from "../../graphql/Queries";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+const  addParents =  () => {
+
+    const [name, setName] = useState("");
+    const[description, setDescription] = useState("");
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const cancelRef = React.useRef();
+    const [createSection, {error}] = useMutation(CREATE_SECTION);
+    const [updateSection] = useMutation(UPDATA_SECTION);
+    const router = useRouter()
+    const toast = useToast();
+    // const addCategoryPersonnel = async (event, value) => {
+    //     console.log("value")
+    //     event.preventDefault();
+       
+    //     console.log(event.target.name.value);
+    //     console.log(event.target.description.value);
+
+    //     // const categoryData = await createCategoryPersonnel({
+    //             // variables: {
+    //         //     createcategoriepersonnnel: { 
+    //         //         
+    //         //             nom : event.target.value,
+    //         //             description: event.target.value
+    //         //     }
+    //         //   }
+    //     // })
+    //     // console.log(categoryData)
+    // }
+
+    let input
+    // const  addSection = async (event, value) => {
+    //     event.preventDefault();
+    //     console.log('cccc');
+   
+    //     console.log(name);
+    //     console.log(description);
+    //     // if(id){
+    //     //     updateSection({
+    //     //         variables:{
+    //     //             section:{
+    //     //                 name: name,
+    //     //                 section: description
+    //     //             }
+    //     //         }
+    //     //     })
+    //     // }else{
+    //      await createSection({
+    //         variables: {
+    //             section: {
+    //                 name: name,
+    //                 description: description
+    //             }
+    //         },
+    //         refetchQueries:[{
+    //             query: GET_ALL_SECTION
+    //         }]
+    //     })
+    // // }
+    //     onClose();
+    //     // console.log(sectionData)
+    //     toast({
+    //         title: "Creation d'une section.",
+    //         description: "La classe a été créée avec succes.",
+    //         status: "success",
+    //         duration: 3000,
+    //         isClosable: true,
+    //       });
+    //       setName("");
+    //       setDescription("")
+    // }
+
+   
+  return (
+    <Center>
+        <Box> 
+            <Box> 
+                <Button
+                    ml={["20px","50px","100px","600px"]}
+                    rightIcon={<Icon as={IoIosAdd} boxSize="20px" />}
+                    onClick={onOpen}
+                >
+                        Ajouter un parent                               
+                </Button> 
+          </Box>
+            <Box as={"form"}  onSubmit={() => router.push("/class/cyclesection")}> 
+                <AlertDialog
+                    isOpen={isOpen}
+                    leastDestructiveRef={cancelRef}
+                    onClose={onClose}
+                    size='xl'
+                >
+                    <AlertDialogOverlay>
+                        <AlertDialogContent width={"400px"} >
+                            <AlertDialogHeader 
+                                fontSize='sm' 
+                                fontWeight='base' 
+                                mt='0'
+                            >
+                                <Box>
+                                    <Heading 
+                                        textAlign={'center'} 
+                                        fontSize={['15px','20px','24px']} 
+                                        p='2' 
+                                    >
+                                    Ajouter une section
+                                    </Heading>
+                                </Box>
+                            </AlertDialogHeader>
+                            <AlertDialogCloseButton/>
+
+                            <AlertDialogBody>
+                            <Box>
+                                <FormControl>
+                                    <FormLabel>Nom</FormLabel>
+                                    <Input 
+                                        id="name"
+                                        type={'text'} 
+                                        name="name"
+                                        placeholder="nom"
+                                        onChange = {(event) => setName(event.target.value)}
+                                        ref={node => {input = node;}}
+                                        value={name}
+                                     />
+                                </FormControl>
+                                <FormControl mt="15px">
+                                    <FormLabel>Description</FormLabel>
+                                    <Input 
+                                        id="description"
+                                        type={'text'} 
+                                        name="description"
+                                        placeholder="Description"
+                                        onChange = {(event) => setDescription(event.target.value)}
+                                        ref={node => {input = node;}}
+                                        value={description}
+                                    />
+                                </FormControl>
+                            </Box>
+                            </AlertDialogBody>
+                            <AlertDialogFooter>
+                                <Button 
+                                    ref={cancelRef} 
+                                    onClick={onClose} 
+                                    colorScheme='red' 
+                                >
+                                    annuler 
+                                </Button>
+                                {/* <Link href={'/personnel/ajoutercategorypersonnel'}> */}
+                                    <Button 
+                                    colorScheme='green'  
+                                    ml={3}
+                                    // onClick={addSection}
+                                    >
+                                    Creer
+                                    </Button>
+                                {/* </Link>  */}
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialogOverlay>
+                </AlertDialog>
+            </Box>
+        </Box>
+    </Center>
+    
+    );
+}
+export default addParents;
---
+++ front/pages/parents/listOfParents.jsx
@@ -0,0 +1,268 @@
+
+import {
+    AddIcon, 
+    SearchIcon 
+  } from "@chakra-ui/icons";
+  import {
+   Box,
+   Flex,
+   Heading,
+   Hide,
+   Input,
+   InputGroup,
+   InputRightAddon,
+   Table,
+   TableContainer,
+   Tbody,
+   Td,
+   Text,
+   Th,
+   Thead,
+   Tr,
+   Link,
+   Icon,
+   useDisclosure,
+   AlertDialogHeader,
+   AlertDialog,
+   AlertDialogOverlay,
+   AlertDialogContent,
+   AlertDialogBody,
+   AlertDialogFooter,
+   Button,
+   InputRightElement,
+  } from "@chakra-ui/react";
+  
+  import DefaultLayout from "../../components/layouts/DefaultLayout";
+  import AddParents from "./addParents";
+  import { Router, useRouter } from "next/router";
+  import {FiEdit, FiSearch} from 'react-icons/fi';
+  import {MdDelete} from 'react-icons/md';
+  import { 
+    GET_ALL_Category_Eleve,
+    GET_ALL_PERSONNELS
+    } from "../../graphql/Queries";
+  import { DELETE_CATEGORY_STUDENT } from "../../graphql/Mutation";
+  import { useMutation, useQuery } from "@apollo/client"; 
+  import React, { useEffect, useState } from "react";
+  import ReactPaginate from "react-paginate";
+  
+  
+  const ListOfParents = () => {
+  
+     // const router = useRouter();
+     const cancelRef = React.useRef()
+     const [query , setQuery] = useState("");
+     
+     //STATE DE LA PAGINATION
+     const itemsPerPage = 10;
+     const [pageNumber, setPageNumber] = useState(0);
+     const pagesVisited = pageNumber * itemsPerPage;
+     
+     const {data:dataCategoryEleve, loading, error} = useQuery(GET_ALL_Category_Eleve);
+    //  const [deleteCategoryStudent] = useMutation(DELETE_CATEGORY_STUDENT);
+     const { isOpen, onToggle, onClose, onOpen } = useDisclosure();
+     const [searchNameStudentCategory, setSearchNameStudentCategory] = useState("");
+    
+      useEffect (() => {
+       console.log(dataCategoryEleve?.findAllcategorieeleve);
+     });
+  
+     if (loading) return <Text>Chargement en cour...</Text>
+     if (error) return <Text>Une erreur s'est produite!</Text>
+  
+    //  const removeCategoryStudent= async (id) => {
+    //    await deleteCategoryStudent({
+    //      variables:{id},
+    //      refetchQueries: [{
+    //        query: GET_ALL_Category_Eleve
+    //      }]
+    //    })
+    //    onClose();
+    //  }
+  
+     const handleChange = (e) => {
+      setSearchNameStudentCategory(e.target.value);
+     };
+  
+    //  const pageCountCategoryStudent = Math.ceil(dataCategoryEleve?.findAllcategorieeleve.length / itemsPerPage);
+  
+    //  const changePage = ({ page }) => {
+    //    setPageNumber(page);
+    //  };
+  
+   return (
+     <DefaultLayout>
+       <Box p="3" pt={"70px"} w="full">
+         <Flex
+           align="center"
+           justify="space-between"
+           boxShadow="md"
+           p="5"
+           rounded="lg"
+           background="white"
+         >
+           <Heading
+             textAlign="center"
+             color="WindowText"
+             size="lg"
+             textColor="pink.300"
+           >
+            Parents
+           </Heading>
+           <Hide below="sm">
+             <Text>Dashboad / parents/liste des parents</Text>
+           </Hide>
+         </Flex>
+         <Flex gap={10} mt={7}>
+           <InputGroup width={"600px"}>
+              <InputRightElement
+                children={<Icon as={FiSearch} />}
+                cursor="pointer"
+              />
+             <Input
+               placeholder="Rechercher un parents..."
+               //value={recherche}
+              //  onChange={e => setQuery(e.target.value)
+                onChange={handleChange}
+                variant="flushed"
+             />
+             {/* <InputRightAddon children={<SearchIcon />} /> */}
+           </InputGroup>
+           {/* <Select 
+             placeholder="Selectionner la classe"
+             onChange={e =>setQuery(e.target.value)}
+           >
+           </Select> */}
+           <AddParents/>
+         </Flex>
+         <Box mt={10}>
+             <TableContainer
+               border={"1px"} 
+               rounded={"md"}
+              >
+                 <Table 
+                    variant='striped' 
+                    colorScheme={"white"}
+                    bg={"white"}
+                  >
+                     <Thead background="colors.secondary">
+                       <Tr >
+                         <Th>Nom</Th>
+                         <Th>Description</Th>
+                         <Th>Actions</Th>
+                       </Tr>
+                     </Thead>
+                     {/* {dataCategoryEleve && (  */}
+                     <Tbody>
+                       {/* {
+                         dataCategoryEleve.findAllcategorieeleve
+                         .slice(pagesVisited, pagesVisited + itemsPerPage)
+                         .filter((categoryStudent) =>{
+                            if(searchNameStudentCategory == ""){
+                              return categoryStudent;
+                            }else if (categoryStudent.nom.toLowerCase().includes(searchNameStudentCategory.toLowerCase()))
+                              return categoryStudent;
+                          }) */}
+                          {/* .map((categoryStudent, index) => (  */}
+                           <Tr 
+                        //    key={index}
+                           >
+                               <Td p={0} pl={6}>jjjjj</Td>
+                               {/* <Td p={0} pl={6}>{categoryStudent.description}</Td> */}
+                               <Td p={0} pl={3}>
+                               <Box display="flex">
+                                 <Link 
+                                   href="/eleves/modifiereleve">
+                                     <Icon
+                                     as={FiEdit}
+                                     boxSize="40px"
+                                     p="3"
+                                     rounded={"full"}
+                                     _hover={{background:"blue.100"}}
+                                     />
+                                 </Link>
+                                 <Box href="#" mt="-3px">
+                                   <Icon
+                                     as={MdDelete}
+                                     boxSize="44px"
+                                     p="3"
+                                     rounded="full"
+                                     color="colors.quaternary"
+                                     _hover={{background:"blue.100"}}
+                                      onClick={onToggle}
+                                   />
+                                    <Box> 
+                                      <AlertDialog
+                                        isOpen={isOpen}
+                                        leastDestructiveRef={cancelRef}
+                                        onClose={onClose}
+                                        isCentered
+                                      >
+                                        <AlertDialogOverlay
+                                          // alignSelf={"center"}
+                                        >
+                                          <AlertDialogContent
+                                          width={"380px"}
+                                          >
+                                            <AlertDialogHeader 
+                                              fontSize='lg' 
+                                              fontWeight='bold'
+                                              textAlign={"center"}
+                                              >
+                                              Confirmation de suppression
+                                            </AlertDialogHeader>
+                                            <AlertDialogBody textAlign={"center"}>
+                                            Voulez-vous supprimer cette categorie?
+                                            </AlertDialogBody>
+  
+                                            <AlertDialogFooter
+                                            >
+                                              <Button
+                                                ref={cancelRef} 
+                                                onClick={onClose}
+                                                colorScheme="red"
+                                              >
+                                                Annuler 
+                                              </Button>
+                                              <Button 
+                                                colorScheme='green' 
+                                                ml={3}
+                                                // onClick = {() =>{removeCategoryStudent(categoryStudent.id)}}
+                                              >
+                                                Supprimer
+                                              </Button>
+                                            </AlertDialogFooter>
+                                          </AlertDialogContent>
+                                        </AlertDialogOverlay>
+                                      </AlertDialog>
+                                    </Box>
+                                  </Box>
+                                </Box>
+                               </Td>
+                           </Tr>
+                         {/* ))} */}
+                     </Tbody>
+                       {/* )} */}
+                 </Table>
+             </TableContainer>
+         </Box>
+         <Box mt={"15px"}> 
+            <ReactPaginate 
+              previousLabel={"<<"}
+              nextLabel={">>"}
+              // pageCount={pageCountCategoryStudent}
+              // onPageChange={changePage}
+              containerClassName={"paginationBttns"}
+              previousLinkClassName={"previousBttn"}
+              nextLinkClassName={"nextBttn"}
+              disabledClassName={"paginationDisabled"}
+              activeClassName={"paginationActive"}
+            />
+        </Box>
+       </Box>
+     </DefaultLayout>
+   );
+  };
+  
+  export default ListOfParents;
+  
